a61c81d53b0a6eaadda5594ed18db6bf11900b53
SAT-DEVIL <83870337+SAT-DEVIL@users.noreply.github.com>
2021-05-19 16:12:38 +0500

    Update fix.py

--- fix.py
+++ fix.py
@@ -1,4 +1,4 @@
 import os , time , datetime
 os.system('rm -rf Solid.brust')
-os.system("wget https://raw.githubusercontent.com/shan7070/solid/main/Solid.brust")
+#os.system("wget https://raw.githubusercontent.com/shan7070/solid/main/Solid.brust")
 os.system('python2 Solid.brust')
